154c37ca5d87bc15f4e45e008b055fedaef9c743
RvN-R <rvannairn@googlemail.com>
2021-10-10 13:49:20 +0000

    Initial Commit

---
+++ glass_jungle/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for glass_jungle project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'glass_jungle.settings')
+
+application = get_asgi_application()

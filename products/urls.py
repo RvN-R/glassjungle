76e1e9c92e8cff0a03d0ba8c23f5cc8472151a3e
RvN-R <rvannairn@googlemail.com>
2021-10-12 09:19:00 +0000

    full_terrariums.html created, changed the cards in both full_terrariums.html and empty_terrarums.html so images act as links

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('empty_terrariums/', views.empty_terrariums, name='empty_terrariums'),
+    path('full_terrariums/', views.full_terrariums, name='full_terrariums'),
 ]
--- products/views.py
+++ products/views.py
@@ -6,12 +6,19 @@ from .models import Product
 
 def empty_terrariums(request):
     """A view to return empty_terrariums.html"""
-    # products = Product.objects.all()
     products = Product.objects.filter(category='empty terrarium')
     context = {
         'products': products,
     }
-
-    print(products)
-
+    
     return render(request, 'products/empty_terrariums.html', context)
+
+
+def full_terrariums(request):
+    """A view to return full_terrariums.html"""
+    products = Product.objects.filter(category='full terrarium')
+    context = {
+        'products': products,
+    }
+    
+    return render(request, 'products/full_terrariums.html', context)

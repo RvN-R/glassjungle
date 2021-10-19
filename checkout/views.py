b614c6f4220086413f3794d50a45aeedc6125eaf
RvN-R <rvannairn@googlemail.com>
2021-10-19 07:40:18 +0000

    Stripe elements added

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JmCQuFry3fofAsosiLoFyRzCcBQeHiXmfzuqXnrLtZTzi9KIKxcjPBpvzKx1YuzamWuk6X1tNqJSDEoxiiuvNa300uBTNhUXG',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)

4af11ea802354bb92117f9ece73106cd9692f81c
RvN-R <rvannairn@googlemail.com>
2021-10-23 15:04:47 +0000

    update_rating.html created, views and url created and the update_rating function completed

--- rating/urls.py
+++ rating/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('create_rating/<product_id>', views.create_rating, name='create_rating'),
+    path('update_rating/<rating_id>', views.update_rating, name='update_rating'),
 ]
--- rating/views.py
+++ rating/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib.auth.models import User
+from django.contrib import messages
 from .models import Rating
 from .forms import CreateRatingForm
 from products.models import Product
@@ -17,24 +18,36 @@ def create_rating(request, product_id):
         'product': product
         }
     if request.method == 'POST':
-        print(request.POST)
-        print(product)
-        print(user)
         form_data = {
-            # "poster": user,
             "comment": request.POST['comment'],
             "rating": request.POST['rating'],
-            # "product_id": product
         }
         rating_form = CreateRatingForm(form_data)
         if rating_form.is_valid():
-            rating= rating_form.save(commit=False)
+            rating = rating_form.save(commit=False)
             rating.poster = request.user
             rating.product_id = product
             rating.updated = datetime.datetime.now()
             rating.created = datetime.datetime.now()
             rating.save()
-            return redirect(reverse('product_detail',args=[product.id]))
+            messages.success(request, 'Rating Successfully added!')
+            return redirect(reverse('product_detail', args=[product.id]))
     
     return render(request, 'rating/create_rating.html', context)
 
+
+def update_rating(request, rating_id):
+    """A view to return update_rating.html"""
+    rating = get_object_or_404(Rating, pk=rating_id)
+    form = CreateRatingForm(instance=rating)
+    context = {
+        'rating': rating,
+        'form': form
+    }
+    if request.method == 'POST':
+        form = CreateRatingForm(request.POST, instance=rating)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Rating Successfully Updated!')
+            return redirect(reverse('product_detail', args=[rating.id]))
+    return render(request, 'rating/update_rating.html', context)

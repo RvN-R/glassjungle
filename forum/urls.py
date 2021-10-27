7beb9207e1a75c5267af02f0ad054a5e4118ea0a
RvN-R <rvannairn@googlemail.com>
2021-10-27 07:06:13 +0000

    Created share_builds.html, added this to the view and urls of forum app

--- forum/urls.py
+++ forum/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('share_builds/', views.share_builds, name='share_builds'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.core.validators import ValidationError
+from django.contrib import messages
+from .models import Forum
 
-# Create your views here.
+
+import datetime
+
+
+def share_builds(request):
+    """ A view to return share_builds page """
+    context = {}
+    return render(request, 'forum/share_builds.html')

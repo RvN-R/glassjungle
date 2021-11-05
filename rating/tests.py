4b65a27db642c4b86d47831711d91ddc47c38612
RvN-R <rvannairn@googlemail.com>
2021-11-05 11:00:49 +0000

    create unit test for rating app, testing CreateRatingForm works correctly

--- rating/tests.py
+++ rating/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .forms import CreateRatingForm
 
-# Create your tests here.
+
+class TestCreateRatingForm(TestCase):
+
+    def test_create_rating_form_valid(self):
+        form = CreateRatingForm(
+            {"comment": "TestComment", "rating": "4"}
+        )
+        self.assertTrue(form.is_valid())
+
+    def test_comment_error_message_returned(self):
+        form = CreateRatingForm(
+            {"comment": "", "rating": "4"}
+        )
+        self.assertFalse(form.is_valid())
+        self.assertIn('comment', form.errors.keys())
+        self.assertEqual(form.errors['comment'], 'Please fill in this field.')

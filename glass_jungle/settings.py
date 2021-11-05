5d5cb38d89ec3bd08a6cf0104dfdeb8164853cd9
RvN-R <rvannairn@googlemail.com>
2021-11-05 16:46:19 +0000

    Added STATIC_ROOT to settings

--- glass_jungle/settings.py
+++ glass_jungle/settings.py
@@ -184,6 +184,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'glass-jungle'

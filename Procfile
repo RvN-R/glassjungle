ee4bf72e48a5d1040d07a9bb75fd8b0827612c4f
RvN-R <rvannairn@googlemail.com>
2021-11-05 12:03:25 +0000

    Attempting to deploy to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn glass_jungle.wsgi:application
--- glass_jungle/settings.py
+++ glass_jungle/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 if os.path.exists("env.py"):
     import env
@@ -29,7 +30,7 @@ SECRET_KEY = 'django-insecure-nqf&3!+&%yv#kf9k1*u3bdre(@lt#j+c6$g2kjhxp4-y$7nikq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['glass-jungle.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -126,12 +127,17 @@ WSGI_APPLICATION = 'glass_jungle.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation

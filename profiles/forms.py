0c05f21042ee41fff9125f020ed7359ce560a35d
RvN-R <rvannairn@googlemail.com>
2021-11-19 11:32:13 +0000

    Attempt to fix the html validation error on profile.html

--- profiles/forms.py
+++ profiles/forms.py
@@ -29,7 +29,7 @@ class UserProfileForm(forms.ModelForm):
                     placeholder = f'{placeholders[field]} *'
                 else:
                     placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+                self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = (
                 'border-black rounded-0 profile-form-input')
             self.fields[field].label = False
